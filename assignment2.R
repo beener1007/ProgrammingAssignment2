4709620a6ee1ee07e034d8bbc46bb545f949f821
beener1007 <mchang3@ncsu.edu>
2014-12-21 18:35:51 -0500

    Create assignment2.R
    The code for Caching the Inverse of a Matrix

---
+++ assignment2.R
@@ -0,0 +1,25 @@
+makeMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) i <<- solve
+        getsolve <- function() i
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+cachesolve <- function(x, ...) {
+        i <- x$getsolve()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setsolve(i)
+        i
+}
