15928d193a28302189315b335d13d909fe83d8bd
Amy <mchang3@ncsu.edu>
2014-12-21 19:31:47 -0500

    Revert "Update cachematrix.R"
    This reverts commit 470273fc277e70aef163b995b4016d7a3670d0b5.

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {  ## set x to be a matrix you given
-        i <- NULL ## set the original inverse matrix to be NULL
-        set <- function(y) { # takes an input matrix
-                x <<- y  # saves the input matrix
-                i <<- NULL  # resets the solve(inverse matrix) to NULL, basically what happens when a new object is generated.
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
         }
-        get <- function() x ## return the value of original matrix
-        setsolve <- function(solve) i <<- solve ## called by cachesolve() during the first cachsolve()
-        getsolve <- function() i  ##this will return the cached value to cachesolve()
-        list(set = set, get = get, ## accessed each time makeCacheMatrix() is called,
+        get <- function() x
+        setsolve <- function(solve) i <<- solve
+        getsolve <- function() i
+        list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
@@ -23,13 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {  ## set x to be a matrix you given
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
- i <- x$getsolve()   # accesses the object 'x' and gets the value of the solve (inverse matrix)
-        if(!is.null(i)) { ### if solve(inverse matrix)  was already cached (not NULL)
-                message("getting cached data") # ... send this message to the console
-                return(i)   # ... and return the solve(inverse matrix) ... "return" ends 
+ i <- x$getsolve()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
         }
-        data <- x$get() # we reach this code only if x$getsolve() returned NULL
-        i <- solve(data, ...) # if m was NULL then we have to calculate the solve(inverse matrix)
-        x$setsolve(i) # store the calculated inverse matrix value in x (see setmean() in makeCacheMatrix
-        i  # return the solve(inverse matrix) to the code that called this function
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setsolve(i)
+        i
 }
